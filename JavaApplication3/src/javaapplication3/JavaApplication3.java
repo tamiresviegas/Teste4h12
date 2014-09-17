202156b0e8c6937dfc05c32116b942a819d42cbd
31326285 <31326285@092945-P31S401.macklabs.br>
2014-09-17 08:24:50 -0300

    Inclusão de mensagem

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -16,5 +16,6 @@ public class JavaApplication3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Olá");
+        int x=1;
     }
 }
